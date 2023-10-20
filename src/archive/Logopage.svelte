c01b3dfa9e6a2ef2b130a67bebe53d7a80fddca5
kolown <kolown@gmail.com>
2023-10-20 20:39:22 +0800

    logopage template

---
+++ src/archive/Logopage.svelte
@@ -0,0 +1,21 @@
+
+<script>
+    let imageUrl = 'https://kolown.net/server2/kolowntext.svg';
+  </script>
+  
+  <style>
+    #container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 100vh;
+    }
+  
+    #image {
+      width: 300px; /* Adjust the width as per your preference */
+    }
+  </style>
+  
+  <div id="container">
+    <img id="image" src={imageUrl} alt="KOLOWN" />
+  </div>
