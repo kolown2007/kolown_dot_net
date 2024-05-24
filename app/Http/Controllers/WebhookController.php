b56d3617442af22939a88166fb659d8ef6ba3827
kolown2007 <kolown@gmail.com>
2024-05-24 14:56:28 +0800

    Controller update

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -3,21 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
-
 
 class WebhookController extends Controller
 {
     public function handleWebhook(Request $request)
     {
-        $githubEvent = $request->header('X-GitHub-Event');
-
-        if ($githubEvent !== 'push') {
-            return response('Invalid event', 400);
-        }
-
         $process = Process::fromShellCommandline('git pull origin inertia');
 
         $process->run();
