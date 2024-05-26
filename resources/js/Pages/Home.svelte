13b6c95837ed778b4fbba4ac9a2bfdf35126827c
kolown2007 <kolown@gmail.com>
2024-05-26 22:14:53 +0800

    edited views

--- resources/js/Pages/Home.svelte
+++ resources/js/Pages/Home.svelte
@@ -1,7 +1,24 @@
 <script>
+   let D = new Date().getFullYear();;
+   let msg =''
+   function sms(){
+      alert("message sent")
+      msg = '';
+   }
   
-  </script>
-  
- <main>
-    <h1>KoloWn 2024 </h1>
+</script>
+
+ <main class = "h-screen flex flex-col justify-between items-center px-4 py-4 bg-black text-red-900 font-mono ">
+    <div>
+      <h1 >KoloWn App {D} </h1> 
+    </div>
+
+    <div class =" h-screen flex flex-col justify-center">
+      <div class="p-4  flex ">
+         <input id="user-input" type="text" placeholder="Type a message" bind:value={msg} class="w-full max-w-96 px-3 py-2 border rounded-l-md focus:outline-none focus:ring-2 focus:ring-red-500">
+         <button id="send-button" class="bg-red-500 text-white px-4 py-2 rounded-r-md hover:bg-red-400 transition duration-300" on:click={sms}>Send</button>
+     </div>
+    </div>
  </main>
+
+ <!-- flex flex-col justify-center -->
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,9 +2,12 @@
 <!DOCTYPE html>
 <html>
   <head>
+   <title>KoloWn App</title>
     <meta charset="utf-8" />
+    <link rel="icon" href="https://kolown.net/server2/kolowntext.svg"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite('resources/js/app.js')
+    @vite('resources/css/app.css')
     @inertiaHead
   </head>
   <body>
@@ -12,3 +15,7 @@
     @inertia
   </body>
 </html>
+
+
+  
+    
