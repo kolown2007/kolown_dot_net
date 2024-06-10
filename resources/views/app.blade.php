4e418b2271af506204a32ce85a9500d4a3b86ea3
kolown2007 <kolown@gmail.com>
2024-06-10 15:06:48 +0800

    favicon change

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
   <head>
    <title>KoloWn App</title>
     <meta charset="utf-8" />
-    <link rel="icon" href="https://kolown.net/server2/kolowntext.svg"/>
+    <link rel="icon" href="{{ asset('favicon.svg') }}"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite('resources/js/app.js')
     @vite('resources/css/app.css')
