db8d02d51aea3d9cdb18bf42a361f45caac5bd5a
kolown2007 <kolown@gmail.com>
2024-06-12 22:12:26 +0800

    api changes

---
+++ config/cors.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel CORS Options
+    |--------------------------------------------------------------------------
+    |
+    | The allowed_methods and allowed_headers options are set to the wildcard
+    | options (*) by default. For most projects, these two options should be
+    | fine. However, if you need to be more specific, you can specify certain
+    | methods or headers.
+    |
+    */
+
+    'paths' => ['api/*', '/storytoday'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+];
