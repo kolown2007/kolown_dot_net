d7fb7b0db96f0cf6f68f591b5fc1e16940c6087a
kolown2007 <kolown@gmail.com>
2024-06-12 14:57:46 +0800

    vite config updates

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import sveltePreprocess from "svelte-preprocess";
 
 export default defineConfig({
     plugins: [
@@ -7,5 +9,8 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-    ],
+        svelte({
+            preprocess: [sveltePreprocess({ typescript: true })],
+        }),
+    ]
 });
