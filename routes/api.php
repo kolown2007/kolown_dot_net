7e6c57178881b1541c1b13e7961671edf60c778f
kolown2007 <kolown@gmail.com>
2024-08-08 22:31:55 +0800

    chore: Add '/date' API route to retrieve current date

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MessageController;
 use App\Http\Controllers\GitController;
 use App\Http\Controllers\GITMcontroller;
+use Carbon\Carbon;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -18,3 +19,8 @@ Route::get('/storymain', [MessageController::class, 'showMainStory']);
 Route::get('/git-pull', [GitController::class, 'pull']);
 
 Route::get('/gitm-data', [GITMcontroller::class, 'gitm']);
+
+
+Route::get('/date', function () {
+    return Carbon::now()->toDateString();
+});
